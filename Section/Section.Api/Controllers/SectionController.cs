3022a3e38abde95f1636f2a9368356f6f3cda02f
radoni02 <108626591+radoni02@users.noreply.github.com>
2024-06-08 13:13:25 +0200

    implemented cqrs on MediatR

---
+++ Section/Section.Api/Controllers/SectionController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Section.Api.Controllers;
+
+[ApiController]
+[Route("api/sections")]
+public class SectionController : ControllerBase
+{
+
+}
---
+++ Section/Section.Application/Abstractions/CQRS/ICommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using Section.Domain.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section.Application.Abstractions.CQRS;
+
+public interface ICommand : IRequest<Result>
+{
+}
+public interface ICommand<TResponse> : IRequest<Result<TResponse>> { }
