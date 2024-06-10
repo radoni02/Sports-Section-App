fd3a4b4ad55db2bba8ea975d4f53e6ed39960102
radoni02 <108626591+radoni02@users.noreply.github.com>
2024-06-10 16:17:22 +0200

    implemented domain models

---
+++ Identity/Identity.Domain/Models/AuthorizationToken.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Identity.Domain.Models;
+
+public sealed class AuthorizationToken
+{
+    [JsonPropertyName("access_token")]
+    public string AccessToken { get; init; } = string.Empty;
+}
